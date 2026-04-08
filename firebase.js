f2bdbd6200313960ad40d06c255562da84ac4e42
mota1 <gustavomotafa@gmail.com>
2026-04-08 14:30:15 -0300

    .

--- firebase.js
+++ firebase.js
@@ -5,9 +5,9 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 // Config do site da sua esposa (NOVO PROJETO)
 const firebaseConfig = {
   apiKey: "AIzaSyC8MeOV36Wm0ZJocnPFN7x5rNqls-kb4eM",
-  authDomain: "sunflower-Beauty-maquiagem.firebaseapp.com",
-  projectId: "sunflower-beauty-maquiagem",
-  storageBucket: "sunflower-beauty-maquiagem.firebasestorage.app",
+  authDomain: "sunflower-Gorgeous-maquiagem.firebaseapp.com",
+  projectId: "sunflower-Gorgeous-maquiagem",
+  storageBucket: "sunflower-Gorgeous-maquiagem.firebasestorage.app",
   messagingSenderId: "616836084056",
   appId: "1:616836084056:web:1c98fda77cbcbdaef752d"
 };
